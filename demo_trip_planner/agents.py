4c0d733671049dcd854a536f4e931996577666ac
Souzai83 <chikushi3.227@gmail.com>
2024-12-11 21:54:44 +1100

    change: set up agents

--- demo_trip_planner/agents.py
+++ demo_trip_planner/agents.py
@@ -13,24 +13,46 @@ class CustomAgents:
         self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
         self.Ollama = Ollama(model="openhermes")
 
-    def agent_1_name(self):
+    def expert_travel_agent(self):
         return Agent(
-            role="Define agent 1 role here",
-            backstory=dedent(f"""Define agent 1 backstory here"""),
-            goal=dedent(f"""Define agent 1 goal here"""),
+            role="Expert Travel Agent",
+            backstory=dedent(f"""
+                            Expert in travel planning and logistics.
+                             I have decades of ezperience making travel itineraries.
+                            """),
+            goal=dedent(f"""
+                        Create a 7-day travel intinerary with detailed per-day plans,
+                        include budget, packing suggestions, and safety tips.
+                        """),
             # tools=[tool_1, tool_2],
             allow_delegation=False,
             verbose=True,
             llm=self.OpenAIGPT35,
         )
 
-    def agent_2_name(self):
+    def city_selection_expert(self):
         return Agent(
-            role="Define agent 2 role here",
-            backstory=dedent(f"""Define agent 2 backstory here"""),
-            goal=dedent(f"""Define agent 2 goal here"""),
+            role="City Selection Expert",
+            backstory=dedent(f"""Expert at analyzing travel data to pick ideal destinations"""),
+            goal=dedent(f"""
+Select the best cities based on weather, season, prices and traveler interests.
+
+"""),
             # tools=[tool_1, tool_2],
             allow_delegation=False,
             verbose=True,
             llm=self.OpenAIGPT35,
         )
+    
+    
+    def local_tour_guide(self):
+        return Agent(
+            role="Local Tour Guide",
+            backstory=dedent(f"""Knowledgable local guide with extensive information about the city, its attractions and customs."""),
+            goal=dedent(f"""Provide the BEST insights about the selected city"""),
+            # tools=[tool_1, tool_2],
+            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
+
